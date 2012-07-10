e29c3ec2e4a7cf3365da624777d9d261fa08d160
Glenn Howard <glennrphoward@gmail.com>
2012-07-10 17:42:14 +0100

    combining 2 tests into the one file.

--- tests/test_view_history_after_log_in.py
+++ tests/test_view_history_after_log_in.py
@@ -17,12 +17,15 @@ class TestViewHistory:
     def test_visitor_can_view_history(self, mozwebqa):
         home_pg = HomePage(mozwebqa)
         home_pg.go_to_home_page()
-        home_pg.personal_tools_region.click_log_in_or_create_account()
 
+        history_pg = home_pg.header_region.click_history()
+        Assert.true(history_pg.is_the_current_page)
+        Assert.greater(len(history_pg.history_list.strip()), 0)
+
+        home_pg.personal_tools_region.click_log_in_or_create_account()
         log_in_or_create_account_pg = LogInOrCreateAccountPage(mozwebqa)
         log_in_or_create_account_pg.log_in()
 
         history_pg = home_pg.header_region.click_history()
-
         Assert.true(history_pg.is_the_current_page)
         Assert.greater(len(history_pg.history_list.strip()), 0)
